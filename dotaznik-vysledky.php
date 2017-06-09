0a6f4eeb00f52e91958102f6e67e05f17dc0cb40
Logotrop <keramdobes@gmail.com>
2017-06-09 17:26:52 -0400

    V.1
    Webovka k 9.6.2017

---
+++ dotaznik-vysledky.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "logotrop";
+$password = "Imthjjdn222333";
+$dbname = "tomellis";
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if ($conn->connection_error) {
+    die("Connection Failed: " .$conn->connection_error);
+}
+$answer1 = $_POST['q1'];
+    $answer2 = $_POST['q2'];
+    $answer3 = $_POST['q3'];
+    $answer4 = $_POST['q4'];
+    $answer5 = $_POST['q5'];
+	$nick = $_POST['nick'];
+   
+    $totalCorrect = 0;
+    
+    if ($answer1 == "A") { $totalCorrect++; }
+    if ($answer2 == "C") { $totalCorrect++; }
+    if ($answer3 == "C") { $totalCorrect++; }
+    if ($answer4 == "B") { $totalCorrect++; }
+    if ($answer5 == "A") { $totalCorrect++; }
+    
+    
+    
+    $sql = "INSERT INTO Dotaznik (nick, q1, q2, q3, q4, q5, qright)
+VALUES ('$nick', '$answer1', '$answer2', '$answer3', '$answer4', '$answer5', $totalCorrect)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Dotaznik byl zaznamenan :]";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?>
+<html lang="cs-CZ">
+    <head>
+        <meta charset="utf-8">
+        <meta name="autor" content="Marek Dobes">
+        <meta name="description" content="Tom Ellis Stranka">
+        <link rel="icon" href="icona.png" />
+        <title>Tom Ellis</title>
+        <link rel="stylesheet" href="styl.css">
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script src="navigace.js"></script>
+    </head>
+    <body id="blok">
+        
+        <h1>Tom Ellis</h1>
+            <div class="nav" id="navigace">
+                <a href="index.php">hlavní</a>
+                <a href="zivot.html">život</a>
+                <a href="kariera.html">Kariéra</a>
+                <a href="serialy.html">Seriály</a>
+                <a href="dotaznik.php">Dotazník</a>
+				<a href="javascript:void(0);"  class="icon" onclick="myFunction()">&#9776;
+            </div>
+        <h2>Tohle je fanpage Velšského herce Toma Ellise</h2>
+
+    <div>
+     <h2>Anonymní dotazník</h2>
+         <form action="dotaznik-vysledky.php" method="post">
+        <h3>Jak jste se dozvěděli o této stránce?</h3>
+        <input type="radio" name="q1" id="q1-A" value="A" />
+        <b><label for="q1A">A) Náhodou </label><br>
+
+        <input type="radio" name="q1" id="q1-B" value="A" />
+        <label for="q1B">B) Náhodou </label><br>
+
+        <input type="radio" name="q1" id="q1-C" value="B" />
+        <label for="q1C">C) Byl jsem donucen v PVY </label></b><br>
+
+        <h3>Jaká role proslavila Toma Ellise?</h3>
+        <input type="radio" name="q2" id="q2-A" value="A" />
+        <label for="q2A">A) Sauron </label><br>
+
+        <input type="radio" name="q2" id="q2-B" value="B" />
+        <label for="q2B">B) Soudkyně Barbara </label><br>
+
+        <input type="radio" name="q2" id="q2-C" value="C" />
+        <b><label for="q2C">C) Lucifer </label></b><br>
+
+        <h3>Kdy se Tom Ellis narodil?</h3>
+        <input type="radio" name="q3" id="q3-A" value="A" />
+        <label for="q3A">A) MCMLOXVI </label><br>
+
+        <input type="radio" name="q3" id="q3-B" value="B" />
+        <label for="q3B">B) MQMLXXVII </label><br>
+
+        <input type="radio" name="q3" id="q3-C" value="C" />
+        <b><label for="q3C">C) MCMLXXVIII </label></b><br>
+
+        <h3>Jak se Tom Ellis jmenuje?</h3>
+        <input type="radio" name="q4" id="q4-A" value="A" />
+        <label for="q4A">A) Veselykralicek1 </label><br>
+
+        <input type="radio" name="q4" id="q4-B" value="B" />
+        <b><label for="q4B">B) Tom Ellis duh.. </label></b><br>
+
+        <input type="radio" name="q4" id="q4-C" value="C" />
+        <label for="q4C">C) Tom Elvis </label><br>
+
+        <h3>Tom Ellis si zahrál v seriálu Dr. Who</h3>
+        <input type="radio" name="q3" id="q3-A" value="A" />
+        <b><label for="q3A">A) ano </label></b><br>
+
+        <input type="radio" name="q3" id="q3-B" value="B" />
+        <label for="q3B">B) ne </label><br>
+
+        <input type="radio" name="q3" id="q3-C" value="C" />
+        <label for="q3C">C) možná, ale kdo to má vědět </label><br><br>
+        
+                </form>
+     <?php
+    
+    
+     
+echo "<h2>$totalCorrect / 5 správně </h2>";
+
+
+$sql = "SELECT id, nick, qright FROM Dotaznik";
+$result = $conn->query($sql);
+echo "<div id='dotaznik'><p>";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Jmeno: " . $row["nick"]. "<br> Počet spravnych odpovědí: " . $row["qright"]. "<br><br>";
+    }
+} else {
+    echo "0 results";
+}
+echo "</p></div>";
+$conn->close();    
+?>
+     
+     </div>
+    <br>
+    <br>
+    <br>
+    
+    
+    <footer>
+
+        <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fgolovan.jecool.net%2F"><h4>Validator</h4></a>
+        <ul>
+        <li><a href="https://en.wikipedia.org/wiki/Tom_Ellis_(actor)">Wikipedia link</a></li>
+        <li><a href="http://www.csfd.cs-CZ/tvurce/15112-tom-ellis/">CSFD bio</a></li>
+        </ul>
+    <b>Více informací na <a href="zivot.html">život</a></b>
+
+    </footer>
+    </body>
+    
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="cs-CZ">
+    <head>
+        <meta charset="utf-8">
+        <meta name="autor" content="Marek Dobes">
+        <meta name="description" content="Tom Ellis Stranka">
+        <link rel="icon" href="icona.png" />
+        <title>Tom Ellis</title>
+        <link rel="stylesheet" href="styl.css">
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script src="navigace.js"></script>
+    </head>
+    <body id="blok">
+        
+        <h1>Tom Ellis</h1>
+            <div class="nav" id="navigace">
+                <a href="index.php">hlavní</a>
+                <a href="zivot.html">život</a>
+                <a href="kariera.html">Kariéra</a>
+                <a href="serialy.html">Seriály</a>
+                <a href="dotaznik.php">Dotazník</a>
+				<a href="javascript:void(0);"  class="icon" onclick="myFunction()">&#9776 </a>
+            </div>
+        <h2>Tohle je fanpage Velšského herce Toma Ellise</h2>
+        
+    <div id="left">
+        <img alt="image" src="https://s-media-cache-ak0.pinimg.com/236x/df/18/5b/df185b4af5e745a0e85d81da5a8372cd.jpg" >
+          
+    </div>
+
+    <div id="telo">
+     <h2>Nejznámější role:</h2>
+     <p>Lucifer<br>V pekle už ho to nebavilo, tak se rozhodl abdikovat na funkci jeho pána a přestěhoval se do Los Angeles, jednoho z nejživějších center USA, kde si otevřel noční klub.</p>
+     <p>Ačkoliv měl Lucifer nejdříve v plánu si pozemský pobyt především užít, velmi brzy mu ho pokazí vražda blízkého přítele.<br>
+     V tu chvíli se hlavní hrdina v podání velšského herce Toma Ellise rozhodne bojovat proti zločinu a přidá se k tamnímu policejnímu útvaru LAPD.</p>       
+    </div>
+    <br>
+    <br>
+    <br>
+    
+    
+    <footer>
+
+        <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fgolovan.jecool.net%2F"><h4>Validator</h4></a>
+        <ul>
+        <li><a href="https://en.wikipedia.org/wiki/Tom_Ellis_(actor)">Wikipedia link</a></li>
+        <li><a href="http://www.csfd.cs-CZ/tvurce/15112-tom-ellis/">CSFD bio</a></li>
+        </ul>
+    <b>Více informací na <a href="zivot.html">život</a></b>
+
+    </footer>
+    </body>
+    
+</html>
